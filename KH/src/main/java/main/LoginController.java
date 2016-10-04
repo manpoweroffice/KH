fbb351e5c8ee6a13b1bc1e25b1ce88318c9178f7
Broad <Broad@DESKTOP-HNGTKAR>
2016-10-04 09:23:47 +0900

    s

--- KH/src/main/java/main/LoginController.java
+++ KH/src/main/java/main/LoginController.java
@@ -1,6 +1,39 @@
 package main;
 
-import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/login/login.do", method = RequestMethod.GET)
+    public String init(Model model) {
+        
+        return "loginForm";
+    }
+ 
+    @RequestMapping(method = RequestMethod.POST)
+    public String submit(Model model, @ModelAttribute("LoginCommand") LoginCommand loginCommand) {
+    	
+    	
+    	
+        if (loginCommand != null && loginCommand.getId() != null & loginCommand.getPassword() != null) {
+            if (loginCommand.getId().equals("test") && loginCommand.getPassword().equals("123")) {
+                model.addAttribute("msg",loginCommand.getId());
+                return "loginsuccess";
+            } else {
+                model.addAttribute("error", "아이디 또는 비밀번호가 다릅니다");
+                return "loginForm";
+            }
+        }
+		return "loginForm"; 
+        }
+    }
+
+
+/*import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -19,7 +52,7 @@ public class LoginController {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String form(){
-		return "Login/loginForm";
+		return "loginForm";
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
@@ -30,4 +63,4 @@ public class LoginController {
 	
 	
 }
-}
+}*/
---
+++ KH/src/main/java/main/LoginDAO.java
@@ -0,0 +1,9 @@
+package main;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+public class LoginDAO extends SqlSessionDaoSupport {
+	
+	public int Login(LoginCommand) 
+
+}
